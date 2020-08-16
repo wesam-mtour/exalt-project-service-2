4684e8b008d61b77cb17c65f808306e5355a93cd
WesamM <wesammtour8@gmail.com>
2020-08-16 15:34:01 +0300

    docker-kafka

---
+++ src/main/java/com/exalt/partssystem/kafka/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.exalt.partssystem.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics="my_topic", groupId="my_group_id")
+    public void getMessage(String message){
+
+        System.out.println(message);
+    }
+}
